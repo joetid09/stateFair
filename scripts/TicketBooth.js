b3ad34c9960992fe73235a8b1ed25c76b3e65a5b
joseph.tidwell09@gmail.com <joseph.tidwell09@gmail.com>
2020-09-04 15:28:14 -0500

    have added counter for food and games

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -2,14 +2,14 @@ const contentTarget = document.querySelector(".entry")
 const eventHub = document.querySelector("#state-fair")
 
 eventHub.addEventListener("click", event => {
-    if (event.target.id == "rideTicket") {
-        const rideEvent = new CustomEvent("rideTicketPurchased", {
-            detail: {
-                ticketPurchased: event.target.value
-            }
-        })
-        eventHub.dispatchEvent(rideEvent)
-    }
+    console.log("if statement passed")
+    const rideEvent = new CustomEvent("rideTicketPurchased", {
+        detail: {
+            ticketPurchased: "ticket"
+        }
+    })
+    eventHub.dispatchEvent(rideEvent)
+    console.log(rideEvent)
 })
 
 export const TicketBooth = () => {
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
 // Import and invoke the ticket booth component function
 import { TicketBooth } from "./TicketBooth.js"
+import { ticketHopeful } from "./rides/RideTicketHolders.js"
 
 TicketBooth()
